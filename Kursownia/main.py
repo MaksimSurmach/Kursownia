6b2c7eaf3fc930a14a46b2f89ca7d11dd232d879
Maksim Surmach <maxsurm@gmail.com>
2023-09-08 13:57:06 +0200

    update bot commands

--- Kursownia/main.py
+++ Kursownia/main.py
@@ -8,7 +8,7 @@ from rates.rate_getter import request_euro_rate, request_dollar_rate
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)  # Outputs debug messages to console.
 
-bot = AsyncTeleBot("TOKEN", parse_mode=None)
+bot = AsyncTeleBot("6693432806:AAGu7LPbIn-Ge98CARF17SvR3m3VGE-hDsQ", parse_mode=None)
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -18,11 +18,12 @@ async def send_welcome(message):
 
 @bot.message_handler(commands=['euro'])
 async def send_euro_rate(message):
-    await message.reply(f'Current euro rate: {request_euro_rate()}')
+    await bot.reply_to(message, f'Current euro rate: {request_euro_rate()}')
+
 
 @bot.message_handler(commands=['dollar'])
 async def send_dollar_rate(message):
-    await message.reply(f'Current dollar rate: {request_dollar_rate()}')
+    await bot.reply_to(message, f'Current dollar rate: {request_dollar_rate()}')
 
 
 
