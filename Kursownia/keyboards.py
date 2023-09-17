29f7c54becb99737bf51c8662f850581996f3215
Maksim Surmach <maxsurm@gmail.com>
2023-09-17 15:48:16 +0200

    add keyboard for choose curr

---
+++ Kursownia/keyboards.py
@@ -0,0 +1,17 @@
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+def generate_keyboard(currency_list: list, amount: float):
+    max_col = 6
+    max_row = 3
+    keyboard = InlineKeyboardMarkup()
+    keyboard.row_width = max_col
+    for i in range(max_row):
+        row = []
+        for j in range(max_col):
+            try:
+                currency = currency_list[i * max_col + j]
+                row.append(InlineKeyboardButton(currency, callback_data=f"{currency} {amount}"))
+            except IndexError:
+                break
+        keyboard.add(*row)
+    return keyboard
